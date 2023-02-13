4f70912c72ee31906742c9367b393839f386b9e8
David Dal Busco <david.dalbusco@outlook.com>
2023-02-13 20:20:54 +0100

    docs: update tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Juno",
-  tagline: "Build web3 apps faster than ever.",
+  tagline: "Build Web3 Apps Faster Than Ever Before.",
   url: "https://juno.build",
   baseUrl: "/",
   onBrokenLinks: "throw",
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,12 +15,12 @@ export default function Home(): JSX.Element {
         <title>{`${siteConfig.title} | Build web3 apps`}</title>
         <link href="https://juno.build" rel="canonical" />
         <meta
-          content="Build web3 apps faster than ever. No backend code required. 100% on-chain with minimal carbon impact."
+          content="Build Web3 Apps Faster Than Ever Before. No backend code required. 100% on-chain with minimal carbon impact."
           name="description"
         />
         <meta content="Juno" property="og:title" />
         <meta
-          content="Build web3 apps faster than ever"
+          content="Build Web3 Apps Faster Than Ever Before."
           property="og:description"
         />
         <meta content="website" property="og:type" />
@@ -32,7 +32,7 @@ export default function Home(): JSX.Element {
         <meta content="summary_large_image" name="twitter:card" />
         <meta content="Juno" name="twitter:title" />
         <meta
-          content="Build web3 apps faster than ever"
+          content="Build Web3 Apps Faster Than Ever Before."
           name="twitter:description"
         />
         <meta
