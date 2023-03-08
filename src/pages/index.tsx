3b7eb563abff1329bef9d06a7e8d7c9a317e5576
Nicolas Mattia <nicolas@nmattia.com>
2023-03-08 13:20:22 +0100

    fix: typo in landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,12 +15,12 @@ export default function Home(): JSX.Element {
         <title>{`${siteConfig.title} | Build web3 apps`}</title>
         <link href="https://juno.build" rel="canonical" />
         <meta
-          content="Build Web3 Apps Like Its Web2. No backend code required. 100% on-chain with minimal carbon impact."
+          content="Build Web3 Apps Like It's Web2. No backend code required. 100% on-chain with minimal carbon impact."
           name="description"
         />
         <meta content="Juno" property="og:title" />
         <meta
-          content="Build Web3 Apps Like Its Web2."
+          content="Build Web3 Apps Like It's Web2."
           property="og:description"
         />
         <meta content="website" property="og:type" />
@@ -32,7 +32,7 @@ export default function Home(): JSX.Element {
         <meta content="summary_large_image" name="twitter:card" />
         <meta content="Juno" name="twitter:title" />
         <meta
-          content="Build Web3 Apps Like Its Web2."
+          content="Build Web3 Apps Like It's Web2."
           name="twitter:description"
         />
         <meta
