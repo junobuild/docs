23be2c733a1181807df879bead9f61ff281742cb
David Dal Busco <david.dalbusco@outlook.com>
2023-04-20 11:55:22 +0200

    style: review

--- src/components/Outro/index.tsx
+++ src/components/Outro/index.tsx
@@ -5,12 +5,9 @@ import Start from "@site/src/components/Start";
 export default function Outro(): JSX.Element {
   return (
     <div className={styles.outro}>
-      <p className={`${styles.sub} ${styles.ease}`}>
-        Develop your next decentralized app with{" "}
-        <span className={styles.bold}>ease</span>.
-      </p>
       <p className={`${styles.sub} ${styles.time}`}>
-        Launch it in <span className={styles.bold}>no time</span>.
+        Develop your next decentralized app in{" "}
+        <span className={styles.bold}>no time</span>.
       </p>
 
       <Start />
