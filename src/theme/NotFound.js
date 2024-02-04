5770aeca77ad8070f8b513563339de259c0905d2
David Dal Busco <david.dalbusco@outlook.com>
2024-02-04 14:43:56 +0100

    fix: 404 for content not found
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -1,9 +1,7 @@
-import Translate, { translate } from "@docusaurus/Translate";
+import { translate } from "@docusaurus/Translate";
 import { PageMetadata } from "@docusaurus/theme-common";
-import JunoLogo from "@site/static/img/logo.svg";
+import { NotFound as NotFoundContent } from "@site/src/components/NotFound";
 import Layout from "@theme/Layout";
-
-import styles from "./notfound.module.scss";
 
 export default function NotFound() {
   return (
@@ -15,40 +13,7 @@ export default function NotFound() {
         })}
       />
       <Layout>
-        <main className="container margin-vert--xl">
-          <div className="row">
-            <div className="col col--6 col--offset-3">
-              <p className={styles.text404}>
-                4<JunoLogo />4
-              </p>
-              <h1 className="hero__title">
-                <Translate
-                  id="theme.NotFound.title"
-                  description="The title of the 404 page"
-                >
-                  Oops! You got lost in space.
-                </Translate>
-              </h1>
-              <p>
-                <Translate
-                  id="theme.NotFound.p1"
-                  description="The first paragraph of the 404 page"
-                >
-                  We could not find what you were looking for.
-                </Translate>
-              </p>
-              <p>
-                <Translate
-                  id="theme.NotFound.p2"
-                  description="The 2nd paragraph of the 404 page"
-                >
-                  Please contact the owner of the site that linked you to the
-                  original URL and let them know their link is broken.
-                </Translate>
-              </p>
-            </div>
-          </div>
-        </main>
+        <NotFoundContent />
       </Layout>
     </>
   );
