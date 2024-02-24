0cd8eede22abbf1b1636acd69272423d4effb68f
David Dal Busco <david.dalbusco@outlook.com>
2024-02-24 09:42:20 +0100

    build: switch to Juno config file TS
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ juno.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "@junobuild/config";
+
+export default defineConfig({
+  satellite: {
+    satelliteId: "ucnx3-aqaaa-aaaal-ab3ea-cai",
+    source: "build"
+  },
+  orbiter: {
+    orbiterId: "3iier-sqaaa-aaaal-aczaa-cai"
+  }
+});
