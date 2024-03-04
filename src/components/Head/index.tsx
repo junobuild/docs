80e54c354a048da483ebf93e0ceb0899c0c6b16c
Francois Best <github@francoisbest.com>
2024-03-04 15:22:48 +0100

    style: replace other tagline instances

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -6,13 +6,13 @@ export default function Home({ children }): JSX.Element {
 
   return (
     <Head>
-      <title>{`${siteConfig.title} | Build Web3 dApps like Web2`}</title>
+      <title>{`${siteConfig.title} | Build Web3 like Web2`}</title>
       <meta
         content="Juno is a zero-knowledge blockchain platform that equips developers with all the essential tools to create any Web3 application, making it as easy as developing serverless Web2."
         name="description"
       />
       <meta content="Juno" property="og:title" />
-      <meta content="Build Web3 dApps like Web2" property="og:description" />
+      <meta content="Build Web3 like Web2" property="og:description" />
       <meta content="website" property="og:type" />
       <meta content="https://juno.build" property="og:url" />
       <meta
@@ -21,7 +21,7 @@ export default function Home({ children }): JSX.Element {
       />
       <meta content="summary_large_image" name="twitter:card" />
       <meta content="Juno" name="twitter:title" />
-      <meta content="Build Web3 dApps like Web2" name="twitter:description" />
+      <meta content="Build Web3 like Web2" name="twitter:description" />
       <meta
         content="https://juno.build/img/social_image_v5.jpg"
         name="twitter:image"
