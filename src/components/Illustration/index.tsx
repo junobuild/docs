9641ab88535234e15ae21e17131156ae241b0920
Francois Best <github@francoisbest.com>
2024-03-04 12:57:24 +0100

    style: add Lottie animation

--- src/components/Illustration/index.tsx
+++ src/components/Illustration/index.tsx
@@ -1,13 +1,38 @@
-import IllustrationSvg from "@site/static/img/illustration.svg";
+import lottie from "lottie-web";
+import React from "react";
+import animationData from "./lottie-animation.json";
 
 import styles from "./styles.module.scss";
 
 const Illustration = () => {
   return (
     <div className={styles.illustration}>
-      <IllustrationSvg />
+      <LottieAnimation />
     </div>
   );
 };
 
 export default Illustration;
+
+// --
+
+function LottieAnimation() {
+  const container = React.useRef(null);
+
+  React.useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: animationData
+    });
+  }, []);
+
+  return (
+    <picture
+      aria-label="An astronaut floating in space around planet Juno orbited by satellites, stars in the background."
+      ref={container}
+    />
+  );
+}
