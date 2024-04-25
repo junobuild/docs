104bf4f5f3f9e333c677d7eab227298b18cb14c5
David Dal Busco <david.dalbusco@outlook.com>
2024-04-25 16:59:32 +0200

    feat: move github actions and local dev to guides (#127)
    * feat: move github actions and local dev to guides
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: redirect moved pages
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- juno.config.ts
+++ juno.config.ts
@@ -3,7 +3,21 @@ import { defineConfig } from "@junobuild/config";
 export default defineConfig({
   satellite: {
     id: "ucnx3-aqaaa-aaaal-ab3ea-cai",
-    source: "build"
+    source: "build",
+    storage: {
+      redirects: [
+        {
+          source: "/docs/miscellaneous/github_actions",
+          location: "/docs/guides/github-actions",
+          code: 301
+        },
+        {
+          source: "/docs/miscellaneous/local-development",
+          location: "/docs/guides/local-development",
+          code: 301
+        }
+      ]
+    }
   },
   orbiter: {
     id: "3iier-sqaaa-aaaal-aczaa-cai"
