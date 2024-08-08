f18fd8124ea45ec2e1bf193bacc58dd26ddf110d
David Dal Busco <david.dalbusco@outlook.com>
2024-08-08 15:08:51 +0200

    feat: integrate mailchimp in react
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -1,42 +1,53 @@
 import styles from "./styles.module.scss";
+import { useFormFields, useMailChimpForm } from "use-mailchimp-form";
 
 export default function Newsletter(): JSX.Element {
+
+  const mailchimpUrl = "https://daviddalbusco.us20.list-manage.com/subscribe/post?u=9b5f83c6cb1006ea8f7225ccb&amp;id=7ec1080aa7&amp;f_id=009126eaf0";
+
+  const {
+    loading,
+    error,
+    success,
+    message,
+    handleSubmit
+  } = useMailChimpForm(mailchimpUrl);
+
+  const { fields, handleFieldChange } = useFormFields({
+    EMAIL: "",
+  });
+
   return (
     <>
       <h2 className={styles.sub}>Newsletter</h2>
       <p>Stay updated with the latest features and significant updates to Juno by reading the occasional newsletter.</p>
-      <form
-        action="https://daviddalbusco.us20.list-manage.com/subscribe/post?u=9b5f83c6cb1006ea8f7225ccb&amp;id=7ec1080aa7&amp;f_id=009126eaf0"
-        method="post"
-        id="mc-embedded-subscribe-form"
-        name="mc-embedded-subscribe-form"
-        target="_self"
-        noValidate={true}
-        className={styles.form}
-      >
-        <input
-          type="email"
-          name="EMAIL"
-          id="mce-EMAIL"
-          required={true}
-          className={styles.input}
-          placeholder="dev@gmail.com"
-        />
 
-        <div
-          style={{ position: "fixed", left: "-5000px" }}
-          aria-hidden="true"
+      <div className={styles.action}>
+        <form
+          className={styles.form}
+          onSubmit={event => {
+            event.preventDefault();
+            handleSubmit(fields);
+          }}
         >
           <input
-            type="text"
-            name="b_9b5f83c6cb1006ea8f7225ccb_7ec1080aa7"
-            tabIndex={-1}
-            defaultValue=""
+            id="EMAIL"
+            type="email"
+            value={fields.EMAIL}
+            onChange={handleFieldChange}
+            className={styles.input}
+            placeholder="dev@gmail.com"
           />
-        </div>
 
-        <button type="submit" className={`${styles.button} button button--hero`}>Subscribe</button>
-      </form>
+          <button type="submit" className={`${styles.button} button button--hero`}>Subscribe</button>
+        </form>
+
+        <p className={styles.msg}>
+          {loading && "Submitting..."}
+          {error && message}
+          {success && message}
+        </p>
+      </div>
     </>
   );
 }
