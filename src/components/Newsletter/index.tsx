24e97a8774b8b8a0c357573c8e55683786f5bbf8
David Dal Busco <david.dalbusco@outlook.com>
2024-08-08 15:12:01 +0200

    chore: format
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -1,31 +1,29 @@
-import styles from "./styles.module.scss";
 import { useFormFields, useMailChimpForm } from "use-mailchimp-form";
+import styles from "./styles.module.scss";
 
 export default function Newsletter(): JSX.Element {
+  const mailchimpUrl =
+    "https://daviddalbusco.us20.list-manage.com/subscribe/post?u=9b5f83c6cb1006ea8f7225ccb&amp;id=7ec1080aa7&amp;f_id=009126eaf0";
 
-  const mailchimpUrl = "https://daviddalbusco.us20.list-manage.com/subscribe/post?u=9b5f83c6cb1006ea8f7225ccb&amp;id=7ec1080aa7&amp;f_id=009126eaf0";
-
-  const {
-    loading,
-    error,
-    success,
-    message,
-    handleSubmit
-  } = useMailChimpForm(mailchimpUrl);
+  const { loading, error, success, message, handleSubmit } =
+    useMailChimpForm(mailchimpUrl);
 
   const { fields, handleFieldChange } = useFormFields({
-    EMAIL: "",
+    EMAIL: ""
   });
 
   return (
     <>
       <h2 className={styles.sub}>Newsletter</h2>
-      <p>Stay updated with the latest features and significant updates to Juno by reading the occasional newsletter.</p>
+      <p>
+        Stay updated with the latest features and significant updates to Juno by
+        reading the occasional newsletter.
+      </p>
 
       <div className={styles.action}>
         <form
           className={styles.form}
-          onSubmit={event => {
+          onSubmit={(event) => {
             event.preventDefault();
             handleSubmit(fields);
           }}
@@ -39,7 +37,12 @@ export default function Newsletter(): JSX.Element {
             placeholder="dev@gmail.com"
           />
 
-          <button type="submit" className={`${styles.button} button button--hero`}>Subscribe</button>
+          <button
+            type="submit"
+            className={`${styles.button} button button--hero`}
+          >
+            Subscribe
+          </button>
         </form>
 
         <p className={styles.msg}>
