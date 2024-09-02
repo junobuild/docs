59420717ef5283262167e2fdf79a8603f29ad858
David Dal Busco <david.dalbusco@outlook.com>
2024-09-02 21:37:25 +0200

    feat: update title and link
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -241,7 +241,7 @@ const config: Config = {
       announcementBar: {
         id: "support_us",
         content:
-          'If you like Juno, <a target="_blank" rel="noopener noreferrer" href="https://github.com/junobuild/juno">star it on GitHub</a>!<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="20" height="20" style="vertical-align: text-bottom;" fill="currentColor"><path d="M480-269 314-169q-11 7-23 6t-21-8q-9-7-14-17.5t-2-23.5l44-189-147-127q-10-9-12.5-20.5T140-571q4-11 12-18t22-9l194-17 75-178q5-12 15.5-18t21.5-6q11 0 21.5 6t15.5 18l75 178 194 17q14 2 22 9t12 18q4 11 1.5 22.5T809-528L662-401l44 189q3 13-2 23.5T690-171q-9 7-21 8t-23-6L480-269Z"/></svg>',
+          '<a aria-label="Learn more about the proposal to transform Juno into a Decentralized Autonomous Organization (DAO)" href="blog/building-tomorrow-juno-dao-for-the-future">Get Involved</a> in Juno’s Decentralization Journey!',
         backgroundColor: "var(--ifm-navbar-background-color)",
         textColor: "var(--ifm-menu-color)",
         isCloseable: false
