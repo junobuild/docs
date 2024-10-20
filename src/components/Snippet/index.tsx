f4d7f5d96d3dc214133f63973ad0d5e2e3144ee4
David Dal Busco <david.dalbusco@outlook.com>
2024-10-20 21:28:28 +0200

    feat: features and snippets more interactive
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/components/Snippet/index.tsx
+++ src/components/Snippet/index.tsx
@@ -9,16 +9,6 @@ export default function Snippet({
 }): JSX.Element {
   return (
     <div className={styles.grid}>
-      <div>
-        <h3 className={styles.title}>{title}</h3>
-
-        {texts.map((text, i) => (
-          <p key={i} className={styles.text}>
-            {text}
-          </p>
-        ))}
-      </div>
-
       <CodeBlock className={lang}>{code}</CodeBlock>
     </div>
   );
