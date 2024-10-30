439a69d7445fd0a6531f23caec55c220d22ec936
David Dal Busco <david.dalbusco@outlook.com>
2024-10-30 17:38:52 +0100

    fix: forgot to use the juno command and not the dev cmd
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- cli-to-md.sh
+++ cli-to-md.sh
@@ -3,7 +3,7 @@
 function generate_command_markdown() {
   local cmd=$1
 
-  node /Users/daviddalbusco/projects/juno/cli/dist/index.js "$cmd" --help --doc | sed 's/\x1b\[[0-9;]*m//g' > docs/miscellaneous/cli/"$cmd".md
+  juno "$cmd" --help --doc | sed 's/\x1b\[[0-9;]*m//g' > docs/miscellaneous/cli/"$cmd".md
 }
 
 COMMANDS=clear,config,deploy,dev,init,login,logout,open,upgrade,use,version,whoami
