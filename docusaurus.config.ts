6b6c38c587214c46c8e0f307375b0a1dc1d79765
David Dal Busco <david.dalbusco@outlook.com>
2024-11-30 10:21:34 +0100

    feat: make announcement bar closable
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -260,10 +260,10 @@ const config: Config = {
       announcementBar: {
         id: "support_us",
         content:
-          'If you like Juno, <a target="_blank" rel="noopener noreferrer" href="https://github.com/junobuild/juno">star it on GitHub</a>!<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor" style="vertical-align: text-bottom;"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143Zm126 18L314-169q-11 7-23 6t-21-8q-9-7-14-17.5t-2-23.5l44-189-147-127q-10-9-12.5-20.5T140-571q4-11 12-18t22-9l194-17 75-178q5-12 15.5-18t21.5-6q11 0 21.5 6t15.5 18l75 178 194 17q14 2 22 9t12 18q4 11 1.5 22.5T809-528L662-401l44 189q3 13-2 23.5T690-171q-9 7-21 8t-23-6L480-269Zm0-201Z"/></svg>',
+          '<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor" style="vertical-align: text-bottom; margin: 0 0.25rem;"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143Zm126 18L314-169q-11 7-23 6t-21-8q-9-7-14-17.5t-2-23.5l44-189-147-127q-10-9-12.5-20.5T140-571q4-11 12-18t22-9l194-17 75-178q5-12 15.5-18t21.5-6q11 0 21.5 6t15.5 18l75 178 194 17q14 2 22 9t12 18q4 11 1.5 22.5T809-528L662-401l44 189q3 13-2 23.5T690-171q-9 7-21 8t-23-6L480-269Zm0-201Z"/></svg>If you like Juno, <a target="_blank" rel="noopener noreferrer" href="https://github.com/junobuild/juno">star it on GitHub</a>',
         backgroundColor: "var(--ifm-navbar-background-color)",
         textColor: "var(--ifm-menu-color)",
-        isCloseable: false
+        isCloseable: true
       }
     }
 };
--- src/theme/AnnouncementBar/index.tsx
+++ src/theme/AnnouncementBar/index.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "@docusaurus/router";
 import { useThemeConfig } from "@docusaurus/theme-common";
+import { useAnnouncementBar } from "@docusaurus/theme-common/internal";
 import AnnouncementBarCloseButton from "@theme/AnnouncementBar/CloseButton";
 import AnnouncementBarContent from "@theme/AnnouncementBar/Content";
 
@@ -7,6 +8,11 @@ import styles from "./styles.module.css";
 
 export default function AnnouncementBar(): JSX.Element | null {
   const { announcementBar } = useThemeConfig();
+
+  const { isActive, close } = useAnnouncementBar();
+  if (!isActive) {
+    return null;
+  }
 
   const { pathname } = useLocation();
   const isHomePage = pathname === "/";
