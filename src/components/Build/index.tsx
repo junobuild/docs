3ddd4a0d97560f8b16f6a9133a1dd33ff0904d75
David Dal Busco <david.dalbusco@outlook.com>
2025-04-01 17:46:04 +0200

    feat: review crafted for web devs
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/components/Build/index.tsx
+++ src/components/Build/index.tsx
@@ -1,9 +1,7 @@
 import Guide from "@site/src/components/Guide";
 import Angular from "@site/static/icons/angular.svg";
 import Astro from "@site/static/icons/astro.svg";
-import JavaScript from "@site/static/icons/javascript.svg";
 import NextJS from "@site/static/icons/nextjs.svg";
-import NodeJS from "@site/static/icons/nodejs.svg";
 import ReactLogo from "@site/static/icons/react.svg";
 import Rust from "@site/static/icons/rust.svg";
 import Svelte from "@site/static/icons/svelte.svg";
@@ -13,10 +11,11 @@ import styles from "./styles.module.scss";
 export default function Build(): JSX.Element {
   return (
     <div className={styles.section}>
-      <h2 className={styles.sub}>Crafted for web devs</h2>
+      <h2 className={styles.sub}>Crafted for Web Devs</h2>
       <p className={styles.build}>
-        There are numerous ways to develop on Juno without sacrificing your
-        preferred tech stack
+        Use the frontend frameworks you{" "}
+        <span className="visually-hidden">love</span>❤️ and write serverless
+        functions in Rust.
       </p>
 
       <div className={styles.grid}>
@@ -25,50 +24,32 @@ export default function Build(): JSX.Element {
           link="/docs/guides/nextjs"
           icon={<NextJS />}
         />
-
         <Guide
           title="Use Juno with React"
           link="/docs/guides/React"
           icon={<ReactLogo />}
         />
-
         <Guide
           title="Use Juno with SvelteKit"
           link="/docs/guides/sveltekit"
           icon={<Svelte />}
         />
-
         <Guide
           title="Use Juno with Vue"
           link="/docs/guides/vue"
           icon={<Vue />}
         />
-
         <Guide
           title="Use Juno with Angular"
           link="/docs/guides/angular"
           icon={<Angular />}
         />
-
         <Guide
           title="Use Juno with Astro"
           link="/docs/guides/astro"
           icon={<Astro />}
         />
-
-        <Guide
-          title="A vanilla JavaScript example showcasing most of Juno's features"
-          link="https://github.com/junobuild/examples/tree/main/vanilla"
-          icon={<JavaScript />}
-          external={true}
-        />
-
-        <Guide
-          title="Use Juno in non-interactive environment with NodeJS."
-          link="/docs/guides/nodejs"
-          icon={<NodeJS />}
-        />
-
+        •
         <Guide
           title="Juno can also be extended with serverless functions written in Rust."
           link="/docs/guides/rust"
