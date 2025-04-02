a3fdc20bb1bb82df87ae3fe502f4caba9213e344
David Dal Busco <david.dalbusco@outlook.com>
2025-04-02 07:17:31 +0200

    feat: order of snippet
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/snippets/functions.ts
+++ src/snippets/functions.ts
@@ -1,7 +1,9 @@
 export const code = {
   lang: "language-rust",
-  value: `// You can supercharge Juno's capabilities
+  value: `// --------------------------------------------------
+// You can supercharge Juno's capabilities
 // by developing serverless features in Rust
+// --------------------------------------------------
 #[on_set_doc(collection = ["notes"])]
 fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
   let data: Person = decode_doc_data(&context.data.data.after.data)?;
@@ -10,7 +12,15 @@ fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
   print(format!("Name: {}", data.name));
 }
 
+// You can also define your own assertions
+#[assert_delete_doc(collections = ["answers"])]
+fn assert_delete_doc(_: AssertDeleteDocContext) -> Result<(), String> {
+    Err("Deletion not authorized".to_string())
+}
+
+// --------------------------------------------------
 // Or write the same logic in TypeScript
+// --------------------------------------------------
 export const onSetDoc = defineHook<OnSetDoc>({
   collections: ["notes"],
   run: async (context) => {
@@ -21,13 +31,6 @@ export const onSetDoc = defineHook<OnSetDoc>({
   }
 });
 
-// You can also define your own assertions
-#[assert_delete_doc(collections = ["answers"])]
-fn assert_delete_doc(_: AssertDeleteDocContext) -> Result<(), String> {
-    Err("Deletion not authorized".to_string())
-}
-
-// Or in JS/TS
 export const assertDeleteDoc = defineAssert<AssertDeleteDoc>({
   collections: ['answers'],
   assert: (context) => {
