823f8981269b03724ae0335768351edef7a51ef2
David Dal Busco <david.dalbusco@outlook.com>
2025-04-22 08:02:27 +0200

    feat: typed title with placeholder (#397)
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,24 +3,36 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Start from "@site/src/components/Start";
 import { trackEvent } from "@site/src/providers/analytics.providers";
 import clsx from "clsx";
-import { ReactTyped } from "react-typed";
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
 import styles from "./styles.module.scss";
 
 export default function Hero(): JSX.Element {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Build", "Run", "Ship"],
+      typeSpeed: 50,
+      backSpeed: 50,
+      backDelay: 3500,
+      loop: true,
+      showCursor: false
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   const { siteConfig } = useDocusaurusContext();
   return (
     <article className={clsx("hero", styles.heroBanner)}>
       <div className={`${styles.container}`}>
         <h1 className={`hero__title ${styles.title} ${styles.item}`}>
-          <ReactTyped
-            strings={["Build", "Ship", "Run"]}
-            typeSpeed={50}
-            backSpeed={50}
-            backDelay={3500}
-            loop
-            cursorChar=""
-            className={styles.typing}
-          />{" "}
+          <span ref={el} className={styles.typing}>
+            Build
+          </span>{" "}
           ideas in
           <br />
           your container
