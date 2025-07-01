d29a323b203611e17a6519921524a94769dc1839
David Dal Busco <david.dalbusco@outlook.com>
2025-07-01 11:58:30 +0200

    feat: iterate
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- plugins/docusaurus.llms.plugin.ts
+++ plugins/docusaurus.llms.plugin.ts
@@ -1,4 +1,5 @@
-import type { LoadContext, Plugin } from "@docusaurus/types";
+import type { LoadContext, Plugin, RouteConfig } from "@docusaurus/types";
+import { flattenRoutes } from "@docusaurus/utils";
 import { existsSync } from "node:fs";
 import { readdir } from "node:fs/promises";
 import { join, relative } from "node:path";
@@ -6,6 +7,7 @@ import { join, relative } from "node:path";
 interface PluginOptions {
   ignorePatterns?: string[];
   docsDir: string;
+  sidebars?: string[];
 }
 
 /**
@@ -20,10 +22,103 @@ export default function docusaurusPluginLLMs(
   return {
     name: "docusaurus-llms-txt-plugin",
 
-    async postBuild(): Promise<void> {
+    async postBuild({ routes, routesBuildMetadata }): Promise<void> {
       console.log("Generating /llms.txt documentation...");
 
-      const options = { docsDir: "docs", ...userOptions };
+      const { sidebars, ...options } = {
+        docsDir: "docs",
+        sidebars: ["docs", "references"],
+        ...userOptions
+      };
+
+      const finalRoutes = flattenRoutes(routes);
+
+      const filteredRoutes = finalRoutes.filter(
+        ({ sidebar }) =>
+          typeof sidebar === "string" && sidebars.includes(sidebar)
+      );
+
+      type RoutePath = string;
+      type TreeRoute = {
+        config: RouteConfig;
+        children?: RouteConfig[];
+      };
+
+      const categoryRoutes = filteredRoutes.filter(
+        ({ component }) => component === "@theme/DocCategoryGeneratedIndexPage"
+      );
+
+      const allTreeRoutes = filteredRoutes.filter((route) => {
+        const isPathTree =
+          filteredRoutes.find(
+            ({ path }) => path.startsWith(route.path) && path !== route.path
+          ) !== undefined;
+
+        return isPathTree;
+      });
+
+      // We flatten the routes. For example if the tree contains both
+      // /docs/build/functions/
+      // and
+      // /docs/build/functions/rust/
+      // as anchors. We keep only the shortest one assuming it is the anchor for the all group.
+      const flattenTreeRoutes = allTreeRoutes.filter(
+        ({ path }) =>
+          allTreeRoutes.find(
+            ({ path: routePath }) =>
+              path.startsWith(routePath) && path !== routePath
+          ) === undefined
+      );
+
+      const treeRoutes = flattenTreeRoutes.reduce<Record<RoutePath, TreeRoute>>(
+        (acc, { path, ...rest }) => {
+          const children = filteredRoutes.filter(
+            ({ path: childPath }) =>
+              childPath.startsWith(path) && childPath !== path
+          );
+
+          return {
+            ...acc,
+            [path]: {
+              config: {
+                path,
+                ...rest
+              },
+              children
+            }
+          };
+        },
+        {}
+      );
+
+      const leafRoutes = filteredRoutes.filter(({ path: routePath }) => {
+        const isTreeRoute =
+          Object.entries(treeRoutes).find(
+            ([treePath, { children }]) =>
+              routePath.startsWith(treePath) ||
+              routePath === treePath ||
+              children.find(({ path }) => path === routePath) !== undefined
+          ) !== undefined;
+
+        const isCategoryRoute =
+          categoryRoutes.find(
+            ({ path: categoryPath }) =>
+              routePath.startsWith(categoryPath) || routePath === categoryPath
+          ) !== undefined;
+
+        return !isTreeRoute && !isCategoryRoute;
+      });
+
+      console.log(leafRoutes);
+
+      // filteredRoutes.forEach((route) => {
+      //   console.log(route);
+      //   if (route.metadata?.sourceFilePath) {
+      //     console.log(
+      //       `Route ${route.path} was created from markdown file ${route.metadata?.sourceFilePath}`
+      //     );
+      //   }
+      // });
 
       const allDocFiles = await collectDocsFiles({ context, options });
     }
