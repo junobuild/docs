9135b37c28cd82396e1d518f5bc2711badf3cd2c
David Dal Busco <david.dalbusco@outlook.com>
2025-07-01 12:30:33 +0200

    feat: custom build routes
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- plugins/docusaurus.llms.plugin.ts
+++ plugins/docusaurus.llms.plugin.ts
@@ -1,11 +1,9 @@
 import type { LoadContext, Plugin, RouteConfig } from "@docusaurus/types";
+import { Props } from "@docusaurus/types/src/context";
 import { flattenRoutes } from "@docusaurus/utils";
-import { existsSync } from "node:fs";
-import { readdir } from "node:fs/promises";
-import { join, relative } from "node:path";
+import { dirname } from "node:path";
 
 interface PluginOptions {
-  ignorePatterns?: string[];
   docsDir: string;
   sidebars?: string[];
 }
@@ -31,155 +29,111 @@ export default function docusaurusPluginLLMs(
         ...userOptions
       };
 
-      const finalRoutes = flattenRoutes(routes);
+      const allRoutes = await collectDocsRoutes({ routes, sidebars });
 
-      const filteredRoutes = finalRoutes.filter(
-        ({ sidebar }) =>
-          typeof sidebar === "string" && sidebars.includes(sidebar)
-      );
-
-      type RoutePath = string;
-      type TreeRoute = {
-        config: RouteConfig;
-        children?: RouteConfig[];
-      };
-
-      const categoryRoutes = filteredRoutes.filter(
-        ({ component }) => component === "@theme/DocCategoryGeneratedIndexPage"
-      );
-
-      const allTreeRoutes = filteredRoutes.filter((route) => {
-        const isPathTree =
-          filteredRoutes.find(
-            ({ path }) => path.startsWith(route.path) && path !== route.path
-          ) !== undefined;
-
-        return isPathTree;
-      });
-
-      // We flatten the routes. For example if the tree contains both
-      // /docs/build/functions/
-      // and
-      // /docs/build/functions/rust/
-      // as anchors. We keep only the shortest one assuming it is the anchor for the all group.
-      const flattenTreeRoutes = allTreeRoutes.filter(
-        ({ path }) =>
-          allTreeRoutes.find(
-            ({ path: routePath }) =>
-              path.startsWith(routePath) && path !== routePath
-          ) === undefined
-      );
-
-      const treeRoutes = flattenTreeRoutes.reduce<Record<RoutePath, TreeRoute>>(
-        (acc, { path, ...rest }) => {
-          const children = filteredRoutes.filter(
-            ({ path: childPath }) =>
-              childPath.startsWith(path) && childPath !== path
-          );
-
-          return {
-            ...acc,
-            [path]: {
-              config: {
-                path,
-                ...rest
-              },
-              children
-            }
-          };
-        },
-        {}
-      );
-
-      const leafRoutes = filteredRoutes.filter(({ path: routePath }) => {
-        const isTreeRoute =
-          Object.entries(treeRoutes).find(
-            ([treePath, { children }]) =>
-              routePath.startsWith(treePath) ||
-              routePath === treePath ||
-              children.find(({ path }) => path === routePath) !== undefined
-          ) !== undefined;
-
-        const isCategoryRoute =
-          categoryRoutes.find(
-            ({ path: categoryPath }) =>
-              routePath.startsWith(categoryPath) || routePath === categoryPath
-          ) !== undefined;
-
-        return !isTreeRoute && !isCategoryRoute;
-      });
-
-      console.log(leafRoutes);
-
-      // filteredRoutes.forEach((route) => {
-      //   console.log(route);
-      //   if (route.metadata?.sourceFilePath) {
-      //     console.log(
-      //       `Route ${route.path} was created from markdown file ${route.metadata?.sourceFilePath}`
-      //     );
-      //   }
-      // });
-
-      const allDocFiles = await collectDocsFiles({ context, options });
+      console.log(routesBuildMetadata);
     }
   };
 }
 
-const collectDocsFiles = async ({
-  context,
-  options
-}: {
-  context: LoadContext;
-  options: PluginOptions;
-}): Promise<string[]> => {
-  const { siteDir, outDir } = context;
-  const { ignorePatterns = [], docsDir } = options;
-
-  const fullDocsDir = join(siteDir, docsDir);
-  const docFiles = await findAllDocsFilePaths({
-    dir: fullDocsDir,
-    ignorePatterns
-  });
-
-  const generatedDocFiles = docFiles.filter((docFile) => {
-    const htmlFile = join(
-      outDir,
-      relative(siteDir, docFile).replace(/\.mdx?$/, "/index.html")
-    );
-    return existsSync(htmlFile);
-  });
-
-  return docFiles;
+type RoutePath = string;
+type TreeRoute = {
+  config?: RouteConfig;
+  children: RouteConfig[];
 };
 
-const findAllDocsFilePaths = async ({
-  dir,
-  ignorePatterns
-}: {
-  dir: string;
-  ignorePatterns?: string[];
-}): Promise<string[]> => {
-  const entries = await readdir(dir, { withFileTypes: true });
+type Routes = Record<RoutePath, TreeRoute>;
 
-  const files: string[] = [];
-  for (const entry of entries) {
-    // Path is deprecated but, current Node types do not provide parentPath
-    // https://github.com/nodejs/node/issues/51955
-    const { name: entryName, path: entryPath } = entry;
-    const fullPath = join(entryPath, entryName);
+const collectDocsRoutes = async ({
+  routes,
+  sidebars
+}: Pick<Props, "routes"> &
+  Required<Pick<PluginOptions, "sidebars">>): Promise<Routes> => {
+  const finalRoutes = flattenRoutes(routes);
 
-    // TODO: ignorePatterns
+  const filteredRoutes = finalRoutes.filter(
+    ({ sidebar }) => typeof sidebar === "string" && sidebars.includes(sidebar)
+  );
 
-    if (entry.isDirectory()) {
-      const subFiles = await findAllDocsFilePaths({
-        dir: fullPath,
-        ignorePatterns
-      });
-      files.push(...subFiles);
-    } else if (entryName.endsWith(".md") || entryName.endsWith(".mdx")) {
-      files.push(fullPath);
-    }
-  }
+  const categoryRoutes = filteredRoutes.filter(
+    ({ component }) => component === "@theme/DocCategoryGeneratedIndexPage"
+  );
 
-  return files;
+  const allTreeRoutes = filteredRoutes.filter((route) => {
+    const isPathTree =
+      filteredRoutes.find(
+        ({ path }) => path.startsWith(route.path) && path !== route.path
+      ) !== undefined;
+
+    return isPathTree;
+  });
+
+  // We flatten the routes. For example if the tree contains both
+  // /docs/build/functions/
+  // and
+  // /docs/build/functions/rust/
+  // as anchors. We keep only the shortest one assuming it is the anchor for the all group.
+  const flattenTreeRoutes = allTreeRoutes.filter(
+    ({ path }) =>
+      allTreeRoutes.find(
+        ({ path: routePath }) =>
+          path.startsWith(routePath) && path !== routePath
+      ) === undefined
+  );
+
+  const categorisedTreeRoutes = flattenTreeRoutes.reduce<Routes>(
+    (acc, { path, ...rest }) => {
+      const children = filteredRoutes.filter(
+        ({ path: childPath }) =>
+          childPath.startsWith(path) && childPath !== path
+      );
+
+      return {
+        ...acc,
+        [path]: {
+          config: {
+            path,
+            ...rest
+          },
+          children
+        }
+      };
+    },
+    {}
+  );
+
+  const leafRoutes = filteredRoutes.filter(({ path: routePath }) => {
+    const isTreeRoute =
+      Object.entries(categorisedTreeRoutes).find(
+        ([treePath, { children }]) =>
+          routePath.startsWith(treePath) ||
+          routePath === treePath ||
+          children.find(({ path }) => path === routePath) !== undefined
+      ) !== undefined;
+
+    const isCategoryRoute =
+      categoryRoutes.find(
+        ({ path: categoryPath }) =>
+          routePath.startsWith(categoryPath) || routePath === categoryPath
+      ) !== undefined;
+
+    return !isTreeRoute && !isCategoryRoute;
+  });
+
+  const leafTreeRoutes = leafRoutes.reduce<Routes>((acc, { path, ...rest }) => {
+    const category = dirname(path);
+
+    return {
+      ...acc,
+      [category]: {
+        ...(acc[category] ?? {}),
+        children: [...(acc[category]?.children ?? []), { path, ...rest }]
+      }
+    };
+  }, {});
+
+  return {
+    ...leafTreeRoutes,
+    ...categorisedTreeRoutes
+  };
 };
