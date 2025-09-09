602dd5e01513255ccd4ae464452bc09d2446f706
David Dal Busco <david.dalbusco@outlook.com>
2025-09-09 15:43:31 +0200

    feat: chatgpt link
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/components/AskAi/index.tsx
+++ src/components/AskAi/index.tsx
@@ -2,6 +2,7 @@ import { useLocation } from "@docusaurus/router";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import IconMarkdown from "@site/static/icons/markdown.svg";
 import IconClaude from "@site/static/icons/claude.svg";
+import IconOpenAI from "@site/static/icons/openai.svg";
 import styles from "./styles.module.scss";
 
 const AskAiLink = ({ icon, text, link, description }) => {
@@ -37,7 +38,10 @@ export const AskAi = () => {
   } = useDocusaurusContext();
 
   const markdownLink = `${pathname}.md`;
-  const claudeLink = `https://claude.ai/new?q=${encodeURIComponent(`Read from this URL: ${url}${pathname}`)}`;
+
+  const prompt = encodeURIComponent(`Read from this URL: ${url}${pathname}`);
+  const claudeLink = `https://claude.ai/new?q=${prompt}`;
+  const chatGPTLink = `https://chatgpt.com/?prompt=${prompt}`;
 
   return (
     <div className="dropdown dropdown--hoverable">
@@ -63,6 +67,13 @@ export const AskAi = () => {
           icon={<IconClaude />}
           link={claudeLink}
           text="Open in Claude"
+          description="Ask questions about this page"
+        />
+
+        <AskAiLink
+          icon={<IconOpenAI />}
+          link={chatGPTLink}
+          text="Open in ChatGPT"
           description="Ask questions about this page"
         />
       </ul>
