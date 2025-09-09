55f6c85bdb894fdc30711aed7c7b14d04870b3ea
David Dal Busco <david.dalbusco@outlook.com>
2025-09-09 14:38:11 +0200

    feat: markdown style link
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/components/AskAi/index.tsx
+++ src/components/AskAi/index.tsx
@@ -1,13 +1,41 @@
-import React from "react";
 import styles from "./styles.module.scss";
+import IconMarkdown from "@site/static/icons/markdown.svg";
+
+const AskAiLink = ({ icon, text, link, description }) => {
+  return (
+    <li>
+      <a href={link} className="dropdown__link">
+        <span className={styles.link}>
+          {icon}
+          <span>{text}</span>
+        </span>
+        <span className={styles.linkDescription}>{description}</span>
+      </a>
+    </li>
+  );
+};
 
 export const AskAi = () => {
+  return (
+    <div className="dropdown dropdown--hoverable">
+      <a
+        href="#"
+        aria-haspopup="true"
+        aria-expanded="false"
+        role="button"
+        className={`navbar__link breadcrumbs__link ${styles.btn}`}
+      >
+        Ask AI
+      </a>
 
-  return <div className="dropdown dropdown--hoverable">
-    <a href="#" aria-haspopup="true" aria-expanded="false" role="button" className={`navbar__link breadcrumbs__link ${styles.btn}`}>Ask AI</a>
-
-    <ul className="dropdown__menu">
-      <li>asdkns</li>
-    </ul>
-  </div>
-}
+      <ul className="dropdown__menu">
+        <AskAiLink
+          icon={<IconMarkdown />}
+          link="/something"
+          text="View as Markdown"
+          description="Open this page in Markdown"
+        />
+      </ul>
+    </div>
+  );
+};
