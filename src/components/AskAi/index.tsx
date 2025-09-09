416e10b631badbf3a0210a4896e230e7d842299a
David Dal Busco <david.dalbusco@outlook.com>
2025-09-09 14:02:47 +0200

    feat: Ask AI
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/components/AskAi/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+export const AskAi = () => {
+
+  return <div className="dropdown dropdown--hoverable">
+    <a href="#" aria-haspopup="true" aria-expanded="false" role="button" className={`navbar__link breadcrumbs__link ${styles.btn}`}>Ask AI</a>
+
+    <ul className="dropdown__menu">
+      <li>asdkns</li>
+    </ul>
+  </div>
+}
